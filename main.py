6f32c33a4b3b332265185b5d431cbf4640eace18
Nihal Chaubey <59233625+chbey@users.noreply.github.com>
2024-06-14 10:19:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import mysql.connector
 conn = mysql.connector.connect(
     host="127.0.0.1",
     user="root",
-    password="Radhakrishna@12345",
+    password="123456",
 )
 
 cursor = conn.cursor()
